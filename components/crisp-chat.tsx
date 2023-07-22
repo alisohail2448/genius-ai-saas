55e6afa568a157aa7be2c2532b7099e0313d7480
sohailali_upcred <sohail.ali@upcred.ai>
2023-07-22 17:33:50 +0530

    completed

---
+++ components/crisp-chat.tsx
@@ -0,0 +1,12 @@
+"use client";
+
+import { useEffect } from "react";
+import { Crisp } from "crisp-sdk-web";
+
+export const CrispChat = () => {
+  useEffect(() => {
+    Crisp.configure("4b84c15c-f1f5-4166-82a2-7618e4d9670a");
+  }, []);
+
+  return null;
+};
